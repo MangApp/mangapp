e7deabc6821bffafef0fe52e77165aba954b24a9
Daniagui12 <d.aguilera@uniandes.edu.co>
2024-06-18 21:25:03 -0500

    overlay toggle working

--- packages/app/features/manga/r/[mangaId]/screen.tsx
+++ packages/app/features/manga/r/[mangaId]/screen.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { YStack, ZStack } from '@my/ui'
 import { SolitoImage as Image } from 'solito/image'
 
 export const MangaReadScreen: React.FC = () => {
+  const [overlayVisible, setOverlayVisible] = useState(true)
+
+  const toggleOverlay = () => {
+    setOverlayVisible(!overlayVisible)
+  }
+
   return (
-    <ZStack flex={1}>
+    <ZStack flex={1} onPress={toggleOverlay}>
       <Image
-        src={
-          'https://cdn.discordapp.com/attachments/1219854791254081649/1247730507043705003/image.png?ex=66611716&is=665fc596&hm=5fcd694796a0ee28abcaca6ac1ed13ba4d08bf97bfdc157e265fa087c270c0f8&'
-        }
+        src={'https://pbs.twimg.com/media/F7O0IBGWsAAplo1?format=jpg'}
         alt={'sapa'}
-        height={100}
-        width={100}
-        contentFit="fill"
+        contentFit="contain"
         unoptimized
+        fill
       />
-      <YStack
-        position="absolute"
-        top={0}
-        bottom={0}
-        right={0}
-        left={0}
-        backgroundColor={'rgba(69,85,117,0.7)'}
-      />
+      {overlayVisible && (
+        <YStack
+          position="absolute"
+          top={0}
+          bottom={0}
+          right={0}
+          left={0}
+          backgroundColor={'rgba(69,85,117,0.7)'}
+        />
+      )}
     </ZStack>
   )
 }
