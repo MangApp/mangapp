6174485bce92d9742341519a2d1c24f518c7ab8f
Panis26 <74617788+Panis26@users.noreply.github.com>
2024-06-25 21:43:06 -0500

    responsive feature

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { NavBar } from '@my/ui/src/components/NavBar'
-import { YStack, XStack, Text, ZStack, Button, Stack } from '@my/ui'
+import { YStack, XStack, Text, ZStack, Button, Stack, useWindowDimensions } from '@my/ui'
 import { SolitoImage as Image } from 'solito/image'
-import { X } from '@tamagui/lucide-icons'
 
 export const HomeScreen: React.FC = () => {
+  const { width } = useWindowDimensions()
+  const isSmallScreen = width < 1280
+
   return (
     <YStack width={'100%'} backgroundColor={'#1B1D24'} flexGrow={1} overflow="hidden">
-      <Text fontSize={40} width={'100%'} padding={20} color={'#FF204E'} textAlign="center">
+      <Text
+        fontSize={isSmallScreen ? 20 : 40}
+        width={'100%'}
+        padding={20}
+        color={'#FF204E'}
+        textAlign="center"
+      >
         MANGAVERSE
       </Text>
 
       <ZStack width={'100%'} backgroundColor={'#1B1D24'} flex={1}>
-        <XStack height={'60%'}>
-          <Stack height={'100%'} width={'70%'}>
+        <XStack height={isSmallScreen ? '50%' : '60%'}>
+          <Stack height={'100%'} width={isSmallScreen ? '100%' : '80%'}>
             <Image
               src={'https://pbs.twimg.com/media/F7O0IBGWsAAplo1?format=jpg'}
               alt={'manga-cover'}
@@ -27,18 +35,23 @@ export const HomeScreen: React.FC = () => {
 
         <YStack width={'100%'} zIndex={999_999}>
           <NavBar />
-          <YStack alignSelf="flex-end" width={'40%'} marginRight={30}>
-            <Text fontSize={'$10'} color={'#ffffff'} textAlign="right">
+          <YStack
+            alignSelf="flex-end"
+            width={isSmallScreen ? '60%' : '40%'}
+            marginRight={isSmallScreen ? 10 : 30}
+          >
+            <Text fontSize={isSmallScreen ? 25 : 40} color={'#ffffff'} textAlign="right">
               Manga name
             </Text>
-            <Text fontSize={'$5'} color={'#ffffff'} textAlign="right">
+            <Text fontSize={isSmallScreen ? 15 : 20} color={'#ffffff'} textAlign="right">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua ut enim ad.
             </Text>
             <Button
-              size="$4"
+              size={isSmallScreen ? 30 : 40}
               backgroundColor={'#FF204E'}
-              width={'30%'}
+              width={isSmallScreen ? '15%' : '30%'}
+              minWidth={60}
               alignSelf="flex-end"
               marginTop={20}
             >
--- packages/ui/src/components/NavBar/index.web.tsx
+++ packages/ui/src/components/NavBar/index.web.tsx
@@ -1,32 +1,39 @@
 import React from 'react'
-import { XStack, Text } from '@my/ui/src'
+import { XStack, Text, useWindowDimensions } from '@my/ui/src'
 import { SolitoImage as Image } from 'solito/image'
 
 export const NavBar: React.FC = () => {
+  const { width } = useWindowDimensions()
+  const isSmallScreen = width < 1280
+
   return (
     <XStack
-      marginLeft={60}
-      marginTop={30}
+      marginLeft={isSmallScreen ? 10 : 60}
+      marginTop={isSmallScreen ? 10 : 30}
       alignItems="center"
-      width={'95%'}
+      width={isSmallScreen ? '100%' : '95%'}
       justifyContent="space-between"
     >
       <XStack alignItems="center" gap={'$5'}>
         <Image
-          src={
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZCMd_DXbWYISW6hO57pSpCntDtZy1Foly2A&s'
-          }
+          src={'https://i.ibb.co/q0WBpm4/logo.png'}
           alt={'sapa'}
-          height={100}
-          width={100}
+          height={isSmallScreen ? 50 : 100}
+          width={isSmallScreen ? 50 : 100}
           contentFit="fill"
           unoptimized
         />
-        <Text fontSize={'$9'} textAlign="center">
-          HOME | SEARCH | MANGA LIST
-        </Text>
+        <XStack flexDirection="column" maxWidth={isSmallScreen ? '60%' : 'auto'}>
+          <Text fontSize={isSmallScreen ? '$5' : '$9'} textAlign="center">
+            HOME | SEARCH | MANGA LIST
+          </Text>
+        </XStack>
       </XStack>
-      <Text fontSize={24} color={'#FF204E'}>
+      <Text
+        fontSize={isSmallScreen ? 16 : 24}
+        color={'#FF204E'}
+        marginRight={isSmallScreen ? '2%' : '1%'}
+      >
         Hi, Pablo Peñaranda
       </Text>
     </XStack>
